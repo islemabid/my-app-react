8e6ea70a7433ecf0e8ac583e0390430240ee2b50
islem abid <islem.abid@enis.tn>
2021-10-13 21:38:01 +0100

    ToDO List

---
+++ src/components/FilterButton.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Filterbutton = (props) => {
+    return (
+        <div>
+            <button
+                type="button"
+                className="btn toggle-btn"
+                aria-pressed={props.isPressed}
+                onClick={() => props.setFilter(props.name)}
+            >
+                <span className="visually-hidden">Show </span>
+                <span>{props.name}</span>
+                <span className="visually-hidden"> tasks</span>
+            </button>
+        </div>
+    );
+}
+
+export default Filterbutton;
